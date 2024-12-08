b706f3de8450ff1828f6957864ff0b2b7003ce40
BugHunter-X <askzenai@gmail.com>
2024-12-08 13:12:05 +0100

    Add bugSolution.sol file

---
+++ bugSolution.sol
@@ -0,0 +1,19 @@
+```solidity
+pragma solidity ^0.8.0;
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+}
+
+contract AddSafeMath {
+  using SafeMath for uint256;
+
+  function add(uint256 a, uint256 b) public pure returns (uint256) {
+    return a.add(b);
+  }
+}
+```
